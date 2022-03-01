07aaa9200de8d4385ca6ea801427e3e30525cd3d
TM111 <42980799+TM111@users.noreply.github.com>
2022-03-01 18:08:19 +0100

    update

--- src/baseline_main.py
+++ src/baseline_main.py
@@ -6,11 +6,11 @@ from options import args_parser
 from models import get_net
 
 args = args_parser()
-args.model='cnn'
+if(args.colab==False):
+    args.model='cnn'
 
 if __name__ == '__main__':
-    if(args.colab==True):
-        args = args_parser()
+    #args = args_parser()
     if args.model == 'cnn':
         a=get_net('LeNet5')
         print(a)
