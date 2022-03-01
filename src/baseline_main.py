13b20f75afa7c22ac92cc0cc0809b79db2678ca7
TM111 <42980799+TM111@users.noreply.github.com>
2022-03-01 16:41:12 +0100

    update

--- src/baseline_main.py
+++ src/baseline_main.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 # Python version: 3.6
 
-
 from options import args_parser
+
+args = args_parser()
+args.model='cnn'
 
 if __name__ == '__main__':
     args = args_parser()
     if args.model == 'cnn':
       print(23)
-      
