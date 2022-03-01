069e5f2b24cd451eab5d4278d93414a3f988f5e6
TM111 <42980799+TM111@users.noreply.github.com>
2022-03-01 16:18:44 +0100

    Update baseline_main.py

--- baseline_main.py
+++ baseline_main.py
@@ -3,16 +3,6 @@
 # Python version: 3.6
 
 
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-
-import torch
-from torch.utils.data import DataLoader
-
-from utils import get_dataset
-from options import args_parser
-from update import test_inference
-from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar
 
 
 if __name__ == '__main__':
