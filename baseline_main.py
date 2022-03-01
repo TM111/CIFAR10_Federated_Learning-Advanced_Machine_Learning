ae51ef95fb5bc24095214cd27d9c79cfe6c50a09
TM111 <42980799+TM111@users.noreply.github.com>
2022-03-01 16:11:13 +0100

    Create baseline_main.py

---
+++ baseline_main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+import torch
+from torch.utils.data import DataLoader
+
+from utils import get_dataset
+from options import args_parser
+from update import test_inference
+from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar
+
+
+if __name__ == '__main__':
+    args = args_parser()
+    if args.model == 'cnn':
+      print(23)
