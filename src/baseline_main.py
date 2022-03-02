51eafab7898356f664ad9477dcbb12bbe028f7aa
TM111 <42980799+TM111@users.noreply.github.com>
2022-03-02 22:49:48 +0100

    Update baseline_main.py

--- src/baseline_main.py
+++ src/baseline_main.py
@@ -9,18 +9,9 @@ import torch.nn as nn
 import time
 import random
 
-args = args_parser()
-if(args.COLAB==False):
-    args.MODEL='LeNet5'
-    args.DEVICE='cpu'
-    args.DISTRIBUTION=3
-    args.CENTRALIZED_MODE=True
 
 if __name__ == '__main__':
-    print(args.CENTRALIZED_MODE)
-    if(args.COLAB==True):
-        args = args_parser()
-    print(args.CENTRALIZED_MODE)
+    args = args_parser()
     if(args.CENTRALIZED_MODE==True):
         args.DISTRIBUTION=1
         args.NUM_CLIENTS=100
