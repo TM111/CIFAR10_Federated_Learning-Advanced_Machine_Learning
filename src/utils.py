82c48308ab2322f221d3ac2999745172fef2d55d
TM111 <42980799+TM111@users.noreply.github.com>
2022-04-13 12:58:58 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -207,8 +207,9 @@ def train_clients(clients):
               clients[i].optimizer.step() # update weights based on accumulated gradients
               
               if(ARGS.ALGORITHM=='SCAFFOLD'):
-                  net_para = clients[i].net.state_dict()
                   clients[i].net = clients[i].net.to('cpu')
+                  net_para = clients[i].net.state_dict()
+                  
                   for key in net_para:
                       print(net_para[key].get_device(),clients[i].c_global[key].get_device(),clients[i].c_local[key].get_device())
                       net_para[key] = net_para[key] - ARGS.LR * (clients[i].c_global[key] - clients[i].c_local[key]) # c_global - c_local (variance reduction)
