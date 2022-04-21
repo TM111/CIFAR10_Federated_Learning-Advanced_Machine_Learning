c7ad8d4aff245d46179172bc38e6f13d60c6f33d
TM111 <42980799+TM111@users.noreply.github.com>
2022-04-21 21:55:57 +0200

    Update baseline_main.py

--- src/baseline_main.py
+++ src/baseline_main.py
@@ -90,7 +90,7 @@ if __name__ == '__main__':
       Server = send_client_updates_to_server_and_aggregate(Server, selected_clients)
     
       #DEBUG: print size,sum_weights,sum_updates for each client
-      debug=1
+      debug=0
       if(debug): print_weights(selected_clients, Server.model)
     
       #SERVER MODEL -> CLIENTS
