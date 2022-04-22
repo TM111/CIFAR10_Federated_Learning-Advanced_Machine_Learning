9de0b915b3c9e0a4b0b421ecfa3f9e9d931f44da
Tommaso Marigo <tommasomarigo@hotmail.it>
2022-04-22 17:06:38 +0200

    update aggregation with SGD server optimizer

--- src/server.py
+++ src/server.py
@@ -1,14 +1,16 @@
 from models import get_net_and_optimizer
 from options import ARGS
 import copy
+import torch
 
 #Server datastructure
 class Server():
     def __init__(self):
         self.model, _ = get_net_and_optimizer()
-        self.previous_updates=None   #vt-1  for FedAvgM algorithm
+        
+        self.optimizer=torch.optim.SGD(self.model.parameters(), lr=1, momentum=ARGS.SERVER_MOMENTUM, weight_decay=ARGS.WEIGHT_DECAY)
+        
         if(ARGS.ALGORITHM=='SCAFFOLD'):
             self.c_global=copy.deepcopy(self.model.state_dict()) # server control variates (SCAFFOLD)
             for key in self.c_global: 
                 self.c_global[key] = self.c_global[key]*0.0
-
